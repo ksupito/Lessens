03ab199bee85c26e4d3b7572d315e90aa29045b1
ksupito <ksyha.laz@tyt.by>
2017-12-15 12:15:35 +0300

    no message

--- Proj1/src/MultithreadingTask1.java
+++ Proj1/src/MultithreadingTask1.java
@@ -1,74 +1,93 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
-/**/
 
 
-public class MultithreadingTask1 implements Runnable{
+
+public class MultithreadingTask1  {
     static String OnePath;
     static double SumMediumValuesFromAllFiles;
     static double FinelValue = 0;
-    static String path = "E://Ksupito1//Projects//Proj1//src//files";
-    public static void main(String[] args) throws Exception{
-        File dir = new File(path);
-        File[] list = dir.listFiles();
-        Thread threads;
+    static String path = ".//src//files";
+    static double FinelSum;
 
-        for(int i = 0; i <list.length; i++){
-            threads =  new Thread(new MultithreadingTask1());
+    public static void main(String[] args) {
+        File directory = new File(path);
+        File[] list = directory.listFiles();
+
+        for (int i = 0; i < list.length; i++) {
+          
+            Thread threads = new Thread(new RunClass());
             OnePath = list[i].toString();
             threads.start();
-            Thread.sleep(50);
+            FinelSum = FinelSumMethod();
+            SumMediumValuesFromAllFiles = SumMediumValuesFromAllFiles + FinelSum;
         }
 
-        FinelValue = SumMediumValuesFromAllFiles/list.length;
+        FinelValue = SumMediumValuesFromAllFiles / list.length;
         System.out.println(FinelValue);
 
     }
-    public double MediumValueFromFile(){
+    public static synchronized double FinelSumMethod(){
         double value = 0;
-        int intForChar = 0;
-        int sum = 0;
-        ArrayList<Integer> ListOfInteger = new ArrayList<>();
         ArrayList<Character> ListOfChar = new ArrayList<>();
+        try{
+            FileInputStream file  = new FileInputStream(OnePath);
 
-        try{FileInputStream file = new FileInputStream (OnePath);
-
-            while (file.available() > 0){
-                int s = file.read();
-                ListOfChar.add((char)s);
-            }
+            while (file.available() > 0) {
+            int s = file.read();
+            ListOfChar.add((char)s);
+        }
             file.close();
 
-            for(int i = 0; i < ListOfChar.size(); i++){
-                if(ListOfChar.get(i) =='\r'){
-                    ListOfInteger.add(intForChar);
-                    intForChar = 0;
-                    i++;
-                }
-                else if(i == ListOfChar.size()-1){  intForChar = intForChar *10 + Character.getNumericValue(ListOfChar.get(i));
-                    ListOfInteger.add(intForChar);}
+            ArrayList<Integer> ListOfInteger = new ArrayList<>(ArrayKsu(ListOfChar));
+            value = Value(ListOfInteger);
 
-                else {
-                    intForChar = intForChar *10 + Character.getNumericValue(ListOfChar.get(i));
-                }
-            }
+        }catch (IOException e){}
+     return value;
+    }
 
-            for (int i = 0 ; i < ListOfInteger.size(); i ++){
-                sum = ListOfInteger.get(i) + sum;
+    public static  ArrayList<Integer> ArrayKsu(ArrayList<Character> Char){
+        ArrayList<Character> ListOfChar = new ArrayList<>(Char);
+        ArrayList<Integer> ListOfInteger = new ArrayList<>();
+        int intForChar = 0;
+
+        for (int i = 0; i < ListOfChar.size(); i++) {
+            if (ListOfChar.get(i) == '\r') {
+                ListOfInteger.add(intForChar);
+                intForChar = 0;
+                i++;
+            } else if (i == ListOfChar.size() - 1) {
+                intForChar = intForChar * 10 + Character.getNumericValue(ListOfChar.get(i));
+                ListOfInteger.add(intForChar);
+            } else {
+                intForChar = intForChar * 10 + Character.getNumericValue(ListOfChar.get(i));
             }
-            value = sum /ListOfInteger.size();
         }
-        catch (Exception e){}
-        return value;}
-
-
-    public void run() {
-        double FinelSum = MediumValueFromFile();
-        SumMediumValuesFromAllFiles = SumMediumValuesFromAllFiles + FinelSum;
+        return  ListOfInteger;
 
     }
 
 
+    public static  double Value(ArrayList<Integer> h){
+        double value;
+        int sum = 0;
+        ArrayList<Integer> ListOfInteger = new ArrayList<>(h);
+
+        for (int i = 0; i < ListOfInteger.size(); i++) {
+            sum = ListOfInteger.get(i) + sum;
+        }
+
+        value = sum / ListOfInteger.size();
+        return value;
+    }
+
 }
+ class RunClass implements Runnable{
+    public void run() {
+    }
+
+
+ }
