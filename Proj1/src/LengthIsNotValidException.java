9ef9f321d0bbfc20f9cd263c49696234acc8cad2
ksupito <ksyha.laz@tyt.by>
2017-12-26 16:36:06 +0300

    no message

--- Proj1/src/LengthIsNotValidException.java
+++ Proj1/src/LengthIsNotValidException.java
@@ -1,20 +1,4 @@
 public class LengthIsNotValidException extends Exception {
-    public LengthIsNotValidException() {
-    }
 
-    public LengthIsNotValidException(String message) {
-        super(message);
-    }
-
-    public LengthIsNotValidException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public LengthIsNotValidException(Throwable cause) {
-        super(cause);
-    }
-
-    public LengthIsNotValidException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
+    public LengthIsNotValidException(String message){super(message);}
 }
--- Proj1/src/Task1.java
+++ Proj1/src/Task1.java
@@ -1,69 +1,71 @@
+import com.oracle.tools.packager.Log;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class Task1 {
     public static void main(String[] args) {
-        new Threads(".//src//files//doc1.txt");
+        Threads thread = new Threads(".//src//files//doc1.txt");
+        thread.start();
+        thread.returnValue();
     }
 
     public static class Threads extends Thread {
         private String path;
         private ArrayList<Long> listOfLong = new ArrayList<>();
-        private FileInputStream file;
-        private BufferedReader reader;
 
         public Threads(String path) {
             this.path = path;
-            start();
-            returnValue();
         }
 
         private ArrayList<Long> readFile() {
             listOfLong = new ArrayList<>();
-            try {
-                file = new FileInputStream(this.path);
-                reader = new BufferedReader(new InputStreamReader(file));
+            try (FileInputStream file = new FileInputStream(this.path);
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(file))) {
                 String stringLineTemp;
                 while ((stringLineTemp = reader.readLine()) != null) {
                     if (stringLineTemp.length() > 10 || stringLineTemp.length() == 0) {
                         try {
-                            throw new LengthIsNotValidException();
-                        } catch (LengthIsNotValidException e) {}
-                    } else {
-                        if (checkWithRegExp(stringLineTemp).find() == true) {
-                            try {
-                                listOfLong.add(Long.parseLong(stringLineTemp));
-                            }catch (NumberFormatException e){}
+                            throw new LengthIsNotValidException(String.format("Length of '%s' is incorrect,the length have to be  less than 10 and greater than 0", stringLineTemp));
+                        } catch (LengthIsNotValidException e) {
+                            e.printStackTrace(System.out);
                         }
-                        else {
-                            try {
-                                throw new Exception();
-                            } catch (Exception e) {}
+                    } else {
+                        try {
+                            if (isDigit(stringLineTemp) == true) {
+                                try {
+                                    listOfLong.add(Long.parseLong(stringLineTemp));
+                                } catch (NumberFormatException e) {
+                                    e.getMessage();
+                                }
+                            }
+                        } catch (StringIsNotPositiveAndInteger e) {
+                            e.printStackTrace(System.out);
                         }
                     }
                 }
             } catch (IOException e) {
-            } finally {
-                try {
-                    if (file != null)
-                        file.close();
-                } catch (IOException e) {}
             }
             return listOfLong;
         }
 
-        private Matcher checkWithRegExp(String temp) {
+        private boolean isDigit(String stringLine) throws StringIsNotPositiveAndInteger {
             Pattern pattern = Pattern.compile("^\\d+$");
-            Matcher matcher = pattern.matcher(temp);
-            return matcher;
+            Matcher matcher = pattern.matcher(stringLine);
+            if (matcher.find() == true) {
+                return true;
+            } else {
+                throw new StringIsNotPositiveAndInteger(String.format("String '%s' is not positive and integer", stringLine));
+            }
         }
 
         private double returnValue() {
-            ArrayList<Long> tempListOfLong = new ArrayList<>(readFile());
-            long count = 0;
             double value;
+            long count = 0;
+            ArrayList<Long> tempListOfLong = new ArrayList<>(readFile());
             for (int i = 0; i < tempListOfLong.size(); i++) {
                 count = count + tempListOfLong.get(i);
             }
