18d8b0eb22794a24e2408a217db6804cc9c5c465
ksupito <ksyha.laz@tyt.by>
2017-12-27 19:58:56 +0300

    no message

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,20 +1,22 @@
-
+import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainClass {
-
+    private static final Logger log = Logger.getLogger(Processor.class.getSimpleName());
 
     public static void main(String[] args) {
+        org.apache.log4j.PropertyConfigurator.configure("src/resources/log4j.properties");
         ArrayList<Long> listOfLong = new ArrayList<>();
         long sum;
         double value;
         Processor processor = new Processor();
         Validator validator = new Validator();
+
         try {
             listOfLong = new ArrayList<>(processor.readFile(".//src//files//doc1.txt"));
         } catch (IOException e) {
-
+            log.error(e.getMessage());
         }
 
         sum = validator.sumDigit(listOfLong);
--- src/Processor.java
+++ src/Processor.java
@@ -1,13 +1,13 @@
+import org.apache.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-
 public class Processor {
     private ArrayList<Long> listOfLong = new ArrayList<>();
-
+    private static final Logger log = Logger.getLogger(Processor.class.getSimpleName());
 
     public ArrayList<Long> readFile(String path) throws IOException {
         Validator validator = new Validator();
@@ -19,10 +19,8 @@ public class Processor {
                 try {
                     if (validator.isDigit(validator.checkLenght(stringLineTemp)) == true) ;
                     listOfLong.add(Long.parseLong(stringLineTemp));
-                } catch (LengthIsNotValidException e) {
-
-                } catch (StringIsNotPositiveAndInteger e) {
-
+                } catch (LengthIsNotValidException | StringIsNotPositiveAndInteger e) {
+                    log.error(e.getMessage());
                 }
             }
             return listOfLong;
