e70c0197fddb57755e97a60c4644a83d76ab4eb6
ksupito <ksyha.laz@tyt.by>
2018-01-08 18:33:15 +0300

    no message

--- HTMLpage/src/HTMLPage.java
+++ HTMLpage/src/HTMLPage.java
@@ -8,28 +8,27 @@ import java.io.PrintWriter;
 
 @WebServlet("/pageHTML")
 public class HTMLPage extends HttpServlet {
-    String title = "A Sample Servlet!";
-    String h1 = "Первый servlet с HTML";
-    String pTag = "Первый servlet с HTML, There was an old owl that lived in an oak. Everyday he saw " +
+    final String TITLE = "A Sample Servlet!";
+    final String H1 = "Первый servlet с HTML";
+    final String P_TAG = "Первый servlet с HTML, There was an old owl that lived in an oak. Everyday he saw " +
             "incidents happening around him. Yesterday he saw a boy helping an old man to carry a heavy basket. " +
             "Today he saw a girl shouting at her mother.he more he saw the less he spoke";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String allText = "<html><head><title>%s</title></head><body>" +
+                "<h1 style=\"color:#008080\", align=\"center\">%s</h1>" +
+                "<p><i>%s</i></p>" +
+                "</body></html>";
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter print = resp.getWriter();
-        print.println("<html><head>");
-        print.println(String.format("<title>%s</title>" , title));
-        print.println("</head>");
-        print.println("<body>");
-        print.println(String.format("<h1 style=\"color:#008080\", align=\"center\">%s</h1>", h1));
-        print.println(String.format("<p><i>%s</i></p>",pTag));
-        print.println("</body></html>");
+        print.println(String.format(allText,TITLE,H1,P_TAG));
+
 
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        doGet(req,resp);
+        doGet(req, resp);
     }
 }
