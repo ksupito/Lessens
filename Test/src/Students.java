f52fef23fffead42991a5798e2aa18947289ce45
ksupito <ksyha.laz@tyt.by>
2018-01-11 13:21:29 +0300

    no message

--- Test/src/Students.java
+++ Test/src/Students.java
@@ -3,8 +3,15 @@ import java.util.*;
 public class Students {
     public static void main(String[] args) {
         Students student = new Students();
-        double[] listOfRaiting = new double[]{2.7, 3.4, 10.0, 2.7, 5.6, 43.5, 43.5};
-        System.out.println(student.calculateMinumalSum(listOfRaiting));
+        System.out.println(student.calculateMinumalSum(args));
+    }
+
+    private double[] stringToDouble(String[] stringListOfRaiting){
+        double[] listOfRaiting = new double[stringListOfRaiting.length];
+        for(int i = 0; i < stringListOfRaiting.length; i++){
+            listOfRaiting[i] = Double.parseDouble(stringListOfRaiting[i]);
+        }
+        return listOfRaiting;
     }
 
     private Map<Double, Integer> groupStudentsByRating(double[] listOfRaiting) {
@@ -19,23 +26,31 @@ public class Students {
         return studentsGrouppedByRating;
     }
 
-    private int calculateMinumalSum(double[] list) {
-        Map<Double, Integer> studentsGrouppedByRating = groupStudentsByRating(list);
-        int sume = 0;
-        int count = 1;
-
-        List<Map.Entry<Double, Integer>> entries = new LinkedList<>(studentsGrouppedByRating.entrySet());
+    private Map<Double, Integer> sortStudentsByRating(Map<Double, Integer> map){
+        List<Map.Entry<Double, Integer>> entries = new LinkedList<>(map.entrySet());
         Collections.sort(entries, new Comparator<Map.Entry<Double, Integer>>() {
             @Override
             public int compare(Map.Entry<Double, Integer> o1, Map.Entry<Double, Integer> o2) {
                 return o1.getKey().compareTo(o2.getKey());
             }
         });
+        Map<Double, Integer> sortedMap = new LinkedHashMap<>();
         for (Map.Entry<Double, Integer> pair : entries) {
+            sortedMap.put(pair.getKey(), pair.getValue());
+        }
+        return sortedMap;
+    }
+
+    private int calculateMinumalSum(String[] list) {
+        Map<Double, Integer> studentsGrouppedByRating = sortStudentsByRating(groupStudentsByRating(stringToDouble(list)));
+        int sum = 0;
+        int count = 1;
+
+        for (Map.Entry<Double, Integer> pair : studentsGrouppedByRating.entrySet()) {
             int value = pair.getValue();
-            sume = sume + count * value;
+            sum = sum + count * value;
             count++;
         }
-        return sume;
+        return sum;
     }
 }
