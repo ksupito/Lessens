66cfffab6ee19d3a991c6633ab6fefa32319fccf
ksupito <ksyha.laz@tyt.by>
2018-01-12 19:10:42 +0300

    no message

--- Test/src/Students.java
+++ Test/src/Students.java
@@ -1,33 +1,40 @@
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Students {
     public static void main(String[] args) {
         Students student = new Students();
-        System.out.println(student.calculateMinumalSum(new String[]{"50", "39", "49", "25", "69", "44", "98", "65"}));
+        System.out.println(student.calculateMinumalSum(new String[]{"50", "39.23234234", "49.33055", "25", "69", "44", "98", "65"}));
     }
 
-    private Queue<Integer> stringToInteger(String[] stringListOfRaiting) {
-        Queue<Integer> queueOfStudents = new LinkedList<>();
+    private Queue<Double> convert(String[] stringListOfRaiting) {
+        Queue<Double> queueOfStudents = new LinkedList<>();
+
         for (int i = 0; i < stringListOfRaiting.length; i++) {
             try {
-                queueOfStudents.add(Integer.parseInt(stringListOfRaiting[i]));
+                if (Double.parseDouble(stringListOfRaiting[i]) < 0) {
+                    throw new IllegalArgumentException("Input data is not valid");
+                }
+                queueOfStudents.add(Math.round(Double.parseDouble(stringListOfRaiting[i]) * 100) / 100.00);
             } catch (NumberFormatException e) {
                 System.out.println("Format the value is incorrect");
+                throw new IllegalArgumentException("Input data is not valid");
             }
         }
         return queueOfStudents;
     }
 
     private int calculateMinumalSum(String[] stringListOfRaiting) {
-        Queue<Integer> queueOfStudents = stringToInteger(stringListOfRaiting);
+        Queue<Double> queueOfStudents = convert(stringListOfRaiting);
         int sum = 0;
         int money = 1;
         if (queueOfStudents.size() == 1) {
             sum = money;
         } else {
-            int firstStudent = queueOfStudents.remove();
+            double firstStudent = queueOfStudents.remove();
             while (!queueOfStudents.isEmpty()) {
-                int nextStudent = queueOfStudents.remove();
+                double nextStudent = queueOfStudents.remove();
+                System.out.println(nextStudent);
                 if (firstStudent > nextStudent) {
                     sum = sum + money + 1;
                 }
