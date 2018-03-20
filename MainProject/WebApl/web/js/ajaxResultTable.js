3b4e3faca77dc924070bb2a8c44786a63e64f59f
DESKTOP-SL13AVS\Ksu <ksyha.laz@tyt.by>
2018-03-20 16:40:10 +0300

    no message

--- MainProject/WebApl/web/js/ajaxResultTable.js
+++ MainProject/WebApl/web/js/ajaxResultTable.js
@@ -1,12 +1,12 @@
 $(document).on('click', 'button', function () {
-    var name1 = $(this).text();
+    var name = $(this).text();
     $.ajax({
         type: 'get',
-        data: {page: name1},
+        data: {page: name},
         url: 'result',
-        success: function (result1) {
+        success: function (result) {
             $("#table").empty();
-            var list = JSON.parse(result1);
+            var list = JSON.parse(result);
             $.each(list, function () {
                 $('#table').append('<tr><td>' + this['id'] + '</td><td>' + this['lastName'] +
                     '</td><td>' + this['firstName'] + '</td><tr>')//}
