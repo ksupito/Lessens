87ae1032e00a191e1b1b46f873153091adafc257
ksupito <ksyha.laz@tyt.by>
2018-04-06 00:04:44 +0300

    Update MainConfig.java

--- SpringProject/src/main/java/config/MainConfig.java
+++ SpringProject/src/main/java/config/MainConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"controller", "repository", "model.domain", "utilities"})
+@ComponentScan(basePackages = {"controller", "repository"})
 public class MainConfig {
 }
