6935f9b51a23274378dde8dedda4866c28760cf0
ksupito <ksyha.laz@tyt.by>
2018-04-11 17:47:26 +0300

    Update UserInfoRepository.java

--- SpringProject/src/main/java/com/example/project/repository/UserInfoRepository.java
+++ SpringProject/src/main/java/com/example/project/repository/UserInfoRepository.java
@@ -1,9 +1,9 @@
 package com.example.project.repository;
 
-        import com.example.project.model.UserInfo;
+import com.example.project.model.UserInfo;
 
-        import java.io.IOException;
-        import java.sql.SQLException;
+import java.io.IOException;
+import java.sql.SQLException;
 
 public interface UserInfoRepository {
     UserInfo getInformation(int idUser) throws ClassNotFoundException, SQLException, IOException;
